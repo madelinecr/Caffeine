df521e16cf9bee15f805714a1b18211fd93e4a71
Blaine Pace <blainepace@gmail.com>
2010-01-06 01:11:35 -0600

    Added editor activity. CaffeineMainActivity fires intent to add new items.

--- src/info/bpace/caffeine/CaffeineMainActivity.java
+++ src/info/bpace/caffeine/CaffeineMainActivity.java
@@ -2,8 +2,12 @@ package info.bpace.caffeine;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.content.Intent;
+
 import android.widget.ArrayAdapter;
+
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
 
 public class CaffeineMainActivity extends ListActivity
@@ -26,6 +30,18 @@ public class CaffeineMainActivity extends ListActivity
 	{
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.list_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case R.id.add_item:
+				Intent mIntent = new Intent("info.bpace.caffeine.action.NEW_ITEM");
+				startActivity(mIntent);
+		}
 		return true;
 	}
 }
---
+++ src/info/bpace/caffeine/EditItemActivity.java
@@ -0,0 +1,15 @@
+package info.bpace.caffeine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditItemActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_item);
+	}
+	
+}
