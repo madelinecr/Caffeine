55e1e35d2f9a619f1ff2a70e5926e92c8adc4aa5
Blaine Pace <blainepace@gmail.com>
2010-01-05 20:56:23 -0600

    Changed default activity to a ListActivity with a placeholder entry.

--- src/info/bpace/caffeine/CaffeineMainActivity.java
+++ src/info/bpace/caffeine/CaffeineMainActivity.java
@@ -1,15 +1,21 @@
 package info.bpace.caffeine;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class CaffeineMainActivity extends Activity
+public class CaffeineMainActivity extends ListActivity
 {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		//setContentView(R.layout.main);
+		
+		ArrayAdapter itemArray = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		itemArray.add("Placeholder");
+		setListAdapter(itemArray);
+		getListView().setTextFilterEnabled(true);
 	}
 }
