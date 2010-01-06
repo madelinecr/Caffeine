5f9d6735b7619e2653bd0ef837bbc32b3d6097e9
Blaine Pace <blainepace@gmail.com>
2010-01-06 03:15:36 -0600

    Added static fields and basic database creation / management methods

--- src/info/bpace/caffeine/DBAdapter.java
+++ src/info/bpace/caffeine/DBAdapter.java
@@ -7,8 +7,21 @@ import android.database.sqlite.SQLiteDatabase;
 public class DBAdapter
 {
 	private static final String DATABASE_NAME = "caffeine_db";
-	private static final int DATABASE_VERSION = 2;
+	private static final String DATABASE_TABLE = "drinks";
+	private static final int DATABASE_VERSION = 1;
 	
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_BODY = "body";
+	public static final String KEY_ROWID = "_id";
+	private static final String TAG = "CaffeineDBAdapter";
+
+	private static final String DATABASE_CREATE =
+ 		"create table notes (_id integer primary key autoincrement, "
+		+ "title text not null, body text not null);";
+	
+	/**
+	 * Private helper class to perform basic database management
+	 */
 	private class DBHelper extends SQLiteOpenHelper
 	{
 		DBHelper(Context context)
@@ -19,13 +32,16 @@ public class DBAdapter
 		@Override
 		public void onCreate(SQLiteDatabase db)
 		{
-			
+			db.execSQL(DATABASE_CREATE);
 		}
 		
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 		{
-			
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+				+ newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS notes");
+			onCreate(db);
 		}
 	}
 	
