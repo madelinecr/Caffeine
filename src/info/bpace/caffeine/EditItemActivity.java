f29221bf05c3a3f384f0813b7e9d68d822c5d233
Blaine Pace <blainepace@gmail.com>
2010-01-06 22:46:15 -0600

    Added very basic submit functionality for EditItemActivity. Needs more work.

--- src/info/bpace/caffeine/EditItemActivity.java
+++ src/info/bpace/caffeine/EditItemActivity.java
@@ -2,14 +2,51 @@ package info.bpace.caffeine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class EditItemActivity extends Activity
 {
+	private EditText mTitleText;
+	private EditText mIngredientsText;
+	private EditText mBodyText;
+	private DBAdapter mDBAdapter;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_item);
+		
+		mDBAdapter = new DBAdapter(this);
+		mDBAdapter.open();
+		mTitleText = (EditText) findViewById(R.id.item_title);
+		mIngredientsText = (EditText) findViewById(R.id.item_ingredients);
+		mBodyText = (EditText) findViewById(R.id.item_body);
+		
+		Button submit = (Button) findViewById(R.id.submit);
+		
+		
+		submit.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View view)
+			{
+				setResult(RESULT_OK);
+				saveState();
+				finish();
+			}
+		});
 	}
 	
+	private void saveState()
+	{
+		String title = mTitleText.getText().toString();
+		String ingredients = mIngredientsText.getText().toString();
+		String body = mBodyText.getText().toString();
+		
+		mDBAdapter.createEntry(title, ingredients, body);
+	}
 }
