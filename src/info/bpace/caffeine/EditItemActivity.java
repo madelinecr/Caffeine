28786973279fbbecc439bfdf2b495aca6f3a0c84
Blaine Pace <blainepace@gmail.com>
2010-01-16 21:13:44 -0600

    Wired up category button to create a MultiChoiceItems AlertDialog.

--- src/info/bpace/caffeine/EditItemActivity.java
+++ src/info/bpace/caffeine/EditItemActivity.java
@@ -9,12 +9,19 @@ import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+
 /**
  * Activity for adding and editting items
  * in the database
  */
 public class EditItemActivity extends Activity implements OnClickListener
 {
+	public static final int DIALOG_CATEGORIES = 0;
+	
 	private EditText mTitle;
 	private EditText mIngredients;
 	private EditText mBody;
@@ -42,6 +49,7 @@ public class EditItemActivity extends Activity implements OnClickListener
 		mTitle = (EditText) findViewById(R.id.item_title);
 		mIngredients = (EditText) findViewById(R.id.item_ingredients);
 		mBody = (EditText) findViewById(R.id.item_body);
+		Button categories = (Button) findViewById(R.id.categories);
 		Button submit = (Button) findViewById(R.id.submit);
 		
 		// check if activity is resuming, and pull id from saved state
@@ -62,7 +70,7 @@ public class EditItemActivity extends Activity implements OnClickListener
 		}
 		
 		populateFields(resuming);
-		
+		categories.setOnClickListener(this);
 		submit.setOnClickListener(this);
 	}
 	
@@ -72,9 +80,45 @@ public class EditItemActivity extends Activity implements OnClickListener
 	@Override
 	public void onClick(View view)
 	{
-		setResult(RESULT_OK);
-		saveState();
-		finish();
+		switch(view.getId())
+		{
+			case R.id.categories:
+				showDialog(DIALOG_CATEGORIES);
+				return;
+			case R.id.submit:
+				setResult(RESULT_OK);
+				saveState();
+				finish();
+				return;
+			default:
+				return;
+		}
+	}
+	
+// -----------------------------------------------------------------------------
+// Private methods
+// -----------------------------------------------------------------------------
+
+	protected Dialog onCreateDialog(int id)
+	{
+		switch(id)
+		{
+			case DIALOG_CATEGORIES:
+				final CharSequence[] options = {"Espresso", "Coffee", "Cold"};
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Drink Categories");
+				
+				builder.setMultiChoiceItems(options, null, new DialogInterface.OnMultiChoiceClickListener()
+				{
+					public void onClick(DialogInterface dialog, int which, boolean isChecked)
+					{
+						
+					}
+				});
+				return builder.create();
+			default:
+				return super.onCreateDialog(id);
+		}
 	}
 
 // -----------------------------------------------------------------------------
